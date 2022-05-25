4c60e42f195025c0411a80625fe654df902ba3c3
Syfe <22247810+itssyfe@users.noreply.github.com>
2022-05-25 15:04:53 +0100

    Skeleton code separated and Tasks 1 - 6
    Signed-off-by: Syfe <22247810+ItsSyfe@users.noreply.github.com>

---
+++ Challenge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Breakthrough
+{
+    class Challenge
+    {
+        protected List<string> Condition;
+        protected bool Met;
+
+        public Challenge()
+        {
+            Met = false;
+        }
+
+        public bool GetMet()
+        {
+            return Met;
+        }
+
+        public List<string> GetCondition()
+        {
+            return Condition;
+        }
+
+        public void SetMet(bool newValue)
+        {
+            Met = newValue;
+        }
+
+        public void SetCondition(List<string> newCondition)
+        {
+            Condition = newCondition;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Breakthrough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Breakthrough ThisGame = new Breakthrough();
+            ThisGame.PlayGame();
+            Console.ReadLine();
+        }
+    }
+}
